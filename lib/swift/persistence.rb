8c0bf9247b2cfacbb0f2d4da2f451eb5bbc7e12e
Bharanee Rathna <deepfryed@gmail.com>
2011-01-11 21:00:15 +1100

    updated readme

--- lib/swift/persistence.rb
+++ lib/swift/persistence.rb
@@ -43,19 +43,18 @@ module Swift
 
     # TODO commit and rollback terminology here might be confusing since its
     #      used in the context of internal states not persisted ones.
-    private
-      def commit
-        cache = @__rel || {}
-        (cache[:hasmany] || {}).each {|name, rel| rel.commit}
-        (cache[:hasone]  || {}).each {|name, rel| rel.commit}
-        self.persisted = true
-      end
+    def commit
+      cache = @__rel || {}
+      (cache[:hasmany] || {}).each {|name, rel| rel.commit}
+      (cache[:hasone]  || {}).each {|name, rel| rel.commit}
+      self.persisted = true
+    end
 
-      def rollback
-        cache = @__rel || {}
-        self.send("#{scheme.header.serial}=", nil) if new? && scheme.header.serial
-        (cache[:hasmany] || {}).each {|name, rel| rel.rollback}
-        (cache[:hasone]  || {}).each {|name, rel| rel.rollback}
-      end
+    def rollback
+      cache = @__rel || {}
+      self.send("#{scheme.header.serial}=", nil) if new? && scheme.header.serial
+      (cache[:hasmany] || {}).each {|name, rel| rel.rollback}
+      (cache[:hasone]  || {}).each {|name, rel| rel.rollback}
+    end
   end
 end
