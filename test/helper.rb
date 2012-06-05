66fee94bac3d2c4652afa43389f748a06d2ab263
Bharanee Rathna <deepfryed@gmail.com>
2012-06-06 09:26:11 +1000

    be proud

--- test/helper.rb
+++ test/helper.rb
@@ -1,11 +1,10 @@
 $:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
-require 'minitest/spec'
-require 'minitest/unit'
+require 'minitest/autorun'
+require 'minitest/pride' if $stdout.tty?
 
 require 'swift'
 require 'swift/migrations'
 require 'swift-more'
 
 Swift.setup :default, Swift::DB::Sqlite3, db: ':memory:'
-MiniTest::Unit.autorun
